314366176e7c7b7503f530c604113cd60075b366
DanP <danp@prim.dev>
2023-10-17 06:07:41 +0200

    api.py forma

--- application_credentials.py
+++ application_credentials.py
@@ -3,7 +3,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.components.application_credentials import AuthorizationServer
 from .const import OAUTH2_AUTHORIZE, OAUTH2_TOKEN
 
-
+    
 async def async_get_authorization_server(hass: HomeAssistant) -> AuthorizationServer:
     """Return authorization server."""
     return AuthorizationServer(
